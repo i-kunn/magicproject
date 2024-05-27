028c0e46bc9e22cfabf7875415a4d8a4c94f0c62
i-kunn <kohei100560@outlook.jp>
2024-05-27 10:01:31 +0900

    Update settings.py configuration

--- magic/magic/settings.py
+++ magic/magic/settings.py
@@ -192,5 +192,5 @@ from django.core.management.utils import get_random_secret_key
 SECRET_KEY = get_random_secret_key()
 try:
     from .local_settings import *
-except:
+except ImportError:
     pass
